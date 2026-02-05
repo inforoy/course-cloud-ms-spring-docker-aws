5bf98369babec2f2cd4ea98cf61c1ba79658aaa1
inforoy <rwcalles@gmail.com>
2026-02-05 18:51:12 -0500

    Configuracion de rutas usando application.properties y application.yml

---
+++ ms-gateway-server/src/test/java/com/rwcalle/springcloud/app/gateway/ms_gateway_server/MsGatewayServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rwcalle.springcloud.app.gateway.ms_gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
