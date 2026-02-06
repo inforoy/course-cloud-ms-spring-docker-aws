f2ab099f33d6175687e5be21c5f15a342a594a68
inforoy <rwcalles@gmail.com>
2026-02-05 22:53:43 -0500

    Requests Predicates Factory que vienen de fabrica v2

--- ms-items/src/main/java/com/rwcalle/springcloud/ms/items/controllers/ItemController.java
+++ ms-items/src/main/java/com/rwcalle/springcloud/ms/items/controllers/ItemController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rwcalle.springcloud.ms.items.models.Item;
@@ -23,7 +25,10 @@ public class ItemController {
     }
 
     @GetMapping
-    public List<Item> list() {
+    public List<Item> list(@RequestParam(name = "name", required = false) String name,
+                            @RequestHeader(name = "token-request", required = false) String tokenRequest) {
+        System.out.println(name);
+        System.out.println(tokenRequest);
         return itemService.findAll();
     }
 
