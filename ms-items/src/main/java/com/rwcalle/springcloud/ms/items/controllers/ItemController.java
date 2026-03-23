59ad22b6aeab1c0d65324e196f878ad8971db99d
inforoy <rwcalles@gmail.com>
2026-03-23 16:56:51 -0500

    actualizando datos

--- ms-items/src/main/java/com/rwcalle/springcloud/ms/items/controllers/ItemController.java
+++ ms-items/src/main/java/com/rwcalle/springcloud/ms/items/controllers/ItemController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +30,7 @@ import com.rwcalle.springcloud.ms.items.services.ItemService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
