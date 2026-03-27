49cddf57d0899e94d9caa5700f345a4441ed153a
inforoy <rwcalles@gmail.com>
2026-03-27 18:46:40 -0500

    Configurando el password encoder BCryptPasswordEncoder

--- ms-users/src/main/java/com/rwcalle/springcloud/ms/users/MsUsersApplication.java
+++ ms-users/src/main/java/com/rwcalle/springcloud/ms/users/MsUsersApplication.java
@@ -3,10 +3,18 @@ package com.rwcalle.springcloud.ms.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MsUsersApplication {
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsUsersApplication.class, args);
