8b4d3e3589a71315b0dce37d9a49414432c61946
inforoy <rwcalles@gmail.com>
2026-03-27 12:52:03 -0500

    crear el proyecto de users

---
+++ ms-users/src/test/java/com/rwcalle/springcloud/ms/users/ms_users/MsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwcalle.springcloud.ms.users.ms_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
