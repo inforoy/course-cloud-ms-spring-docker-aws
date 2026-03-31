713b0cc8a863f3d515d4019266f957a594975e0d
inforoy <rwcalles@gmail.com>
2026-03-31 17:51:00 -0500

    Creando el proyecto de SpringSecurity con Oauth2

---
+++ ms-oauth/src/test/java/com/rwcalle/springcloud/ms/oauth/ms_oauth/MsOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwcalle.springcloud.ms.oauth.ms_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
