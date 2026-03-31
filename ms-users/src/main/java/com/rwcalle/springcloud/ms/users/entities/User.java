ea3cfce1f6c2ebfd630bde25cb6f21d5790b3509
inforoy <rwcalles@gmail.com>
2026-03-31 12:25:05 -0500

    Asignando ROLE_ADMIN

--- ms-users/src/main/java/com/rwcalle/springcloud/ms/users/entities/User.java
+++ ms-users/src/main/java/com/rwcalle/springcloud/ms/users/entities/User.java
@@ -13,6 +13,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -33,6 +34,9 @@ public class User {
     private String passwordString;
 
     private Boolean enabled;
+
+    @Transient
+    private boolean admin;
 
     @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
     @ManyToMany
@@ -99,6 +103,14 @@ public class User {
 
     public void setRoles(List<Role> roles) {
         this.roles = roles;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
     }
    
 }
