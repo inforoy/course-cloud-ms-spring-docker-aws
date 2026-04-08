0c6ceb210a1f16a658f7bcdceea539cc9f4f4924
inforoy <rwcalles@gmail.com>
2026-04-07 19:22:01 -0500

    Fix errores - Agragando Logs en Global Filter Gateway

---
+++ ms-products/src/main/java/com/rwcalle/springcloud/ms/products/config/TracingConfig.java
@@ -0,0 +1,39 @@
+package com.rwcalle.springcloud.ms.products.config;
+
+import brave.Tracing;
+import brave.context.slf4j.MDCScopeDecorator;
+import brave.propagation.ThreadLocalCurrentTraceContext;
+import brave.sampler.Sampler;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.brave.bridge.BraveCurrentTraceContext;
+import io.micrometer.tracing.brave.bridge.BraveTracer;
+import io.micrometer.tracing.handler.DefaultTracingObservationHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TracingConfig {
+
+    @Bean
+    public Tracing braveTracing() {
+        var currentTraceContext = ThreadLocalCurrentTraceContext.newBuilder()
+                .addScopeDecorator(MDCScopeDecorator.get())
+                .build();
+        return Tracing.newBuilder()
+                .currentTraceContext(currentTraceContext)
+                .sampler(Sampler.ALWAYS_SAMPLE)
+                .build();
+    }
+
+    @Bean
+    public Tracer micrometerTracer(Tracing tracing) {
+        BraveCurrentTraceContext braveCurrentTraceContext =
+                new BraveCurrentTraceContext(tracing.currentTraceContext());
+        return new BraveTracer(tracing.tracer(), braveCurrentTraceContext);
+    }
+
+    @Bean
+    public DefaultTracingObservationHandler tracingObservationHandler(Tracer tracer) {
+        return new DefaultTracingObservationHandler(tracer);
+    }
+}
