2a5b1cc7b786d54be17e6ee301f2001af13bd450
inforoy <rwcalles@gmail.com>
2026-04-09 12:26:14 -0500

    Moviendo configuracion del filtro de CircuitBreaker de yml a JavaConfig para el Gateway MVC

--- ms-gateway-server/src/main/java/com/rwcalle/springcloud/app/gateway/MsGatewayServerApplication.java
+++ ms-gateway-server/src/main/java/com/rwcalle/springcloud/app/gateway/MsGatewayServerApplication.java
@@ -2,6 +2,15 @@ package com.rwcalle.springcloud.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.stripPrefix;
+import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @SpringBootApplication
 public class MsGatewayServerApplication {
@@ -10,6 +19,17 @@ public class MsGatewayServerApplication {
 
 		SpringApplication.run(MsGatewayServerApplication.class, args);
 		
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routeConfig() {
+		return route("ms-products").route(path("/api/products/**"), http())
+		.filter(lb("ms-products"))
+		.filter(circuitBreaker(config -> config
+			.setId("products")
+			.setStatusCodes("500")
+			.setFallbackPath("forward:/api/items/5")))
+		.before(stripPrefix(2)).build();
 	}
 
 }
