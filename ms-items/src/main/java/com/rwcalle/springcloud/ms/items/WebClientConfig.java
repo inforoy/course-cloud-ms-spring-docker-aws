35d6f70908ab89a035db99241e2d06ca78b37190
inforoy <rwcalles@gmail.com>
2026-04-09 13:23:28 -0500

    Propagando contexto de trazas entre microservicios con auto-configured WebClient

--- ms-items/src/main/java/com/rwcalle/springcloud/ms/items/WebClientConfig.java
+++ ms-items/src/main/java/com/rwcalle/springcloud/ms/items/WebClientConfig.java
@@ -1,7 +1,8 @@
 package com.rwcalle.springcloud.ms.items;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,9 +14,19 @@ public class WebClientConfig {
     @Value("${config.baseurl.endpoint.ms_products}")
     private String url;
 
-    @Bean
+    /*
+    /@Bean
     @LoadBalanced
     WebClient.Builder webClient(){
         return WebClient.builder().baseUrl(url);
     }
+    */
+
+    @Bean
+    WebClient webClient(
+        //WebClient.Builder webClientBuilder,
+        ReactorLoadBalancerExchangeFilterFunction lbFunction){
+        return WebClient.builder().baseUrl(url).filter(lbFunction).build();
+    }
+
 }
